b868b986c5d49d2dda266fa3f02c3f34ec5ef540
Luke Mason <lukesamuelmason@gmail.com>
2021-12-29 08:24:38 -0700

    detail view finished

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Luke Mason on 12/29/21.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,17 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Luke Mason on 12/29/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
+
+// MARK: - NOTES
+// Codable -
+// Identifiable - 
