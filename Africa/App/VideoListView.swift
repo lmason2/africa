5d73c9c6b98024d73dc4fc250ddb1019c0ad92a4
Luke Mason <lukesamuelmason@gmail.com>
2021-12-29 10:30:57 -0700

    Videos finished

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -17,9 +17,11 @@ struct VideoListView: View {
         NavigationView {
             List {
                 ForEach(videos) { item in
-                    VideoListItemView(video: item)
-                        .padding(.vertical, 8)
-                }
+                    NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)) {
+                        VideoListItemView(video: item)
+                            .padding(.vertical, 8)
+                    }
+                } //: LOOP
             } //: LIST
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos", displayMode: .inline)
@@ -30,14 +32,14 @@ struct VideoListView: View {
                         hapticImpact.impactOccurred()
                     }) {
                         Image(systemName: "arrow.2.squarepath")
-                    }
-                }
-            }
+                    } //: BUTTON
+                } //: TOOLBARITEM
+            } //: TOOLBAR
         } //: NAVIGATION
     }
 }
 
-// MARK: - PREVIEw
+// MARK: - PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Luke Mason on 12/29/21.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(filename: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: filename, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: filename, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
