d8ac3d4a8a13b96c4801f51d6c20c634dd3797fd
Luke Mason <lukesamuelmason@gmail.com>
2021-12-29 10:12:33 -0700

    Video Screen set up

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - PROPERTIES
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION
     }
 }
 
+// MARK: - PREVIEw
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
