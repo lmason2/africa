23bb529f44f2bddfa47f28b60e4b2e3ab8ab045b
Luke Mason <lukesamuelmason@gmail.com>
2021-12-29 10:40:39 -0700

    map started

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Luke Mason on 12/29/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    let id: String
+    let name: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
