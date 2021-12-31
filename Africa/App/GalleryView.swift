a96e422f5aa93f6bcb3fd8802aa0fbe36eb20e35
Luke Mason <lukesamuelmason@gmail.com>
2021-12-31 14:15:51 -0700

    gallery done

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct GalleryView: View {
     // MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
     // SIMPLE GRID DEFINITION
     //let gridLayout: [GridItem] = [
@@ -19,20 +21,56 @@ struct GalleryView: View {
     //]
     
     // EFFICIENT GRID DEFINITION
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYNAMIC GRID LAYOUT
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                } //: LOOP
-            } //: GRID
+            // MARK: - IMAGE
+            Image(selectedAnimal)
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 8))
+            
+            // MARK: - SLIDER
+            Slider(value: $gridColumn, in: 2...4, step: 1)
+                .padding(.horizontal)
+                .onChange(of: gridColumn, perform: { value in
+                    gridSwitch()
+                    haptics.impactOccurred()
+                })
+            
+            // MARK: - GRID
+            VStack(alignment: .center, spacing: 30) {
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                } //: GRID
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACk
+            .padding(.horizontal, 10)
+            .padding(.vertical, 15)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
